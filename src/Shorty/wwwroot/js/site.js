820772577c01ccbdb6f4192748d44772eab295be
britto <jose@britto.tech>
2024-07-24 22:35:36 -0400

    expand when clicked outside  mobile menu drawer

--- src/Shorty/wwwroot/js/site.js
+++ src/Shorty/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+window.onload = function() {
+    window.addEventListener('click', function (e) {
+        if (!document.getElementById('mobile-drawer').contains(e.target)) {
+            document.getElementById('menu-btn').classList.remove('open');
+            document.getElementById('mobile-nav').classList.remove('slide-in');
+            document.getElementById('mobile-nav').classList.add('slide-out');
+            document.getElementById('container-noboot-shrinker').classList.remove('shrink');
+        }
+    });
+}
