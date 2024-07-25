cb3fbea0cfa649aec22691ca75de2eeaeace7fb3
britto <jose@britto.tech>
2024-07-24 23:31:37 -0400

    extract menu script into site.js

--- src/Shorty/wwwroot/js/site.js
+++ src/Shorty/wwwroot/js/site.js
@@ -1,15 +1,41 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿let menu_btn;
+let mobile_drawer;
+let mobile_nav;
+let container;
 
 window.onload = function() {
+    menu_btn = document.getElementById('menu-btn');
+    mobile_drawer = document.getElementById('mobile-drawer');
+    mobile_nav = document.getElementById('mobile-nav');
+    container = document.getElementById('container-noboot-shrinker');
+    
     window.addEventListener('click', function (e) {
-        if (!document.getElementById('mobile-drawer').contains(e.target)) {
-            document.getElementById('menu-btn').classList.remove('open');
-            document.getElementById('mobile-nav').classList.remove('slide-in');
-            document.getElementById('mobile-nav').classList.add('slide-out');
-            document.getElementById('container-noboot-shrinker').classList.remove('shrink');
+        if (!mobile_drawer.contains(e.target)) {
+            menu_btn.classList.remove('open');
+            mobile_nav.classList.remove('slide-in');
+            mobile_nav.classList.add('slide-out');
+            container.classList.remove('shrink');
         }
     });
+}
+
+function shrink_content() {
+    menu_btn.classList.add('open');
+    mobile_nav.classList.add('slide-in');
+    mobile_nav.classList.remove('slide-out');
+    container.classList.add('shrink');
+}
+
+function expand_content(){
+    menu_btn.classList.remove('open');
+    mobile_nav.classList.remove('slide-in');
+    mobile_nav.classList.add('slide-out');
+    container.classList.remove('shrink');
+}
+
+function toggle_shrink(){
+    if(menu_btn.classList.contains('open'))
+        expand_content();
+    else 
+        shrink_content();
 }
