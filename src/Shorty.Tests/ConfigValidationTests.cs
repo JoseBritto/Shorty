6412bbe01d2afc26ecdc44505785ea95773af397
britto <jose@britto.tech>
2024-08-18 15:19:46 -0400

    Remove deprecated tests

--- src/Shorty.Tests/ConfigValidationTests.cs
+++ src/Shorty.Tests/ConfigValidationTests.cs
@@ -6,34 +6,5 @@ namespace Shorty.Tests;
 
 public class ConfigValidationTests
 {
-    [Fact]
-    public void EnsureUrlSettingsConfigValidity_ThrowsExceptionWhenShortUrlPrefixIsWhitespace()
-    {
-        // Arrange
-        var config = new ConfigurationBuilder().AddInMemoryCollection(new Dictionary<string, string>
-        {
-            {ConfigVar.SHORT_URL_PREFIX, " "}
-        }).Build();
-        
-        // Act
-        Assert.Throws<ArgumentException>(() => ConfigHelper.EnsureUrlSettingsConfigValidity(config));
-    }
-    
-    [Theory]
-    [InlineData("htt://example.com")]
-    [InlineData("no://mehost")]
-    [InlineData("ftp://example.com")]
-    public void EnsureUrlSettingsConfigValidity_ThrowsExceptionWhenShortUrlPrefixIsNotHttporHttps(string prefix)
-    {
-        // Arrange
-        var config = new ConfigurationBuilder().AddInMemoryCollection(new Dictionary<string, string>
-        {
-            {ConfigVar.SHORT_URL_PREFIX, prefix}
-        }).Build();
-        
-        // Act
-        Assert.Throws<ConfigException>(() => ConfigHelper.EnsureUrlPrefixIsValid(config));
-    }
-    
     
 }
